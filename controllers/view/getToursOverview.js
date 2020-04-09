ed95b7576509fc61ed760d72fd4d0fbf70ae8030
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-04-09 14:37:22 +0100

    Built up the tour overview page with the data gotten from the tour api using the pug templating engine for rendering

--- controllers/view/getToursOverview.js
+++ controllers/view/getToursOverview.js
@@ -1,9 +1,16 @@
+const {Tour} = require('../../models/Tours');
 
-const getToursOverview = (req, res, next) => {
+const asyncWrapper = require('../../middlewares/asyncWrapper');
+
+
+const getToursOverview = asyncWrapper( async(req, res, next) => {
+    const tours = await Tour.find();
+
     res.status(200).render('overview', {
-        title: 'Exciting tours for adventurous people'
+        title: 'Exciting tours for adventurous people',
+        tours
     })
-};
+});
 
 
 module.exports = getToursOverview;
