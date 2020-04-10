43d26b487adce5aa3bf2d748cd479ee66b2162e8
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-04-10 22:35:58 +0100

    Integrated mapbox map visualization into the tour details page

--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,3 +1,37 @@
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 
-console.log(locations)
+mapboxgl.accessToken = 'pk.eyJ1IjoibWljaGFlbG9ndW5zYW5taSIsImEiOiJjazVjdWFqczcwYmUzM3BxZnlmdjVjZjByIn0.5-kccAXdV326Iz9KBt7sRg';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/michaelogunsanmi/ck8uo04zm11x91imgm0jld1ez',
+    scrollZoom: false
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach(location => {
+    const htmlElement = document.createElement('div');
+    htmlElement.className = 'marker';
+
+    new mapboxgl.Marker({
+        element: htmlElement,
+        anchor: 'bottom'
+    }).setLngLat(location.coordinates).addTo(map);
+
+    new mapboxgl.Popup({
+        offset: 30
+    }).setLngLat(location.coordinates)
+        .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
+        .addTo(map);
+
+    bounds.extend(location.coordinates);
+});
+
+map.fitBounds(bounds, {
+    padding: {
+        top: 200,
+        bottom: 150,
+        left: 100,
+        right: 100
+    }
+});
