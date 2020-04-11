f1e07249ba8f8ede7e0d9f4b551e08fd00e40511
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-04-11 15:17:45 +0100

    Finished up on the login views route

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
 
+import {showAlert} from "./alerts";
+
 
 export const handleLoginFormSubmit = async event => {
     event.preventDefault();
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
-    const response = await axios({
-        method: 'POST',
-        url: '/api/v1/users/login',
-        data: {
-            email,
-            password
-        }
-    });
+    try {
+        const response = await axios({
+            method: 'POST',
+            url: '/api/v1/users/login',
+            data: {
+                email,
+                password
+            }
+        });
 
+        if (response.data.status === 'success') {
+            showAlert('success', 'Logged In Successfully');
+            window.setTimeout( () => location.assign('/'), 1500);
+        }
+    } catch(error) {
+        showAlert('error', error.response.data.message);
+    }
 };
