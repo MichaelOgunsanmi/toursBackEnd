6104726a06cad216b77d153a8f9a35786dd86389
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-04-12 22:00:17 +0100

    Added the ability to save picture name to the database from the updateme route. Also edited the user model to take in a default image

--- controllers/user/updateUser.js
+++ controllers/user/updateUser.js
@@ -20,6 +20,7 @@ const updateUser = asyncWrapper( async (req, res, next) => {
     });
 
     const filteredBody = filterObject(req.body, 'name', 'email');
+    if (req.file) filteredBody.photo = req.file.filename;
 
     const user = await User.findByIdAndUpdate(req.user._id, filteredBody, {
         new: true,
