aa05b89f1590c63cfd5a64ce66107ccb4e95a2f9
Ogunsanmi <ogunsanmimichael@gmail.com>
2020-04-19 13:44:25 +0100

    Implemented cors

--- server.js
+++ server.js
@@ -10,11 +10,5 @@ const server = app.listen(PORT, () => {
     winston.info(`Listening on port ${PORT}...`);
 });
 
-process.on('SIGTERM', () => {
-    winston.info(`new deployment here`);
-    server.close(() => {
-        winston.info(`Process terminated lll`);
-    });
-});
 
 module.exports = server;
--- startup/security.js
+++ startup/security.js
@@ -1,7 +1,10 @@
 const helmet = require('helmet');
+const cors = require('cors');
 
 
 module.exports = function (app) {
+    app.use(cors());
+    app.options('*', cors());
     app.use(helmet());
     app.enable('trust proxy');
 };
